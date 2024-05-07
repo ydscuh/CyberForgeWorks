8f3f0ff1e3bc27b58fcfd6a5a9de22feaa5b1b1e
ydscuh <gs1evfadden@hotmail.com>
2024-05-07 12:48:13 +0700

    demo sdk

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function hasCycle(head) {
-  let slow = head;
-  let fast = head;
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-    if (slow === fast) return true;
+function mergeKLists(lists) {
+  if (lists.length === 0) return null;
+  while (lists.length > 1) {
+    const first = lists.shift();
+    const second = lists.shift();
+    const merged = mergeTwoLists(first, second);
+    lists.push(merged);
   }
-  return false;
+  return lists[0];
 }
